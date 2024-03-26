9feaecdb93807f3cb406ec5ce05d2fb0de0d1fa0
aashishkumarnayak <kumaraashish57627@gmail.com>
2024-03-27 00:35:28 +0530

    .

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://localhost:5000"
 
-export const BASE_URL = "https://dns-manager-api.onrender.com"
+export const BASE_URL = "https://dns-manager-api-backend.onrender.com"
